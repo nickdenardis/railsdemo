debf5e49ca755886f3634e897d1af7f9896d21f1
Nick DeNardis <nick.denardis@gmail.com>
2012-12-04 20:44:56 -0500

    Added the ability add a site.

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,5 +1,12 @@
 class SitesController < ApplicationController
   def index
+  	@site = Site.new
   	@site_list = Site.all
+  end
+
+  def create
+  	Site.create params[:site]
+  	#render :text => params.inspect
+  	redirect_to :back
   end
 end
--- spec/requests/sites_spec.rb
+++ spec/requests/sites_spec.rb
@@ -10,5 +10,14 @@ describe "Sites" do
     	visit sites_path
     	page.should have_content 'http://wayne.edu/'
     end
+
+  	it "add a new site" do
+  		visit sites_path
+  		fill_in 'Url', :with => 'http://msu.edu/'
+  		click_button 'Add Site'
+
+  		current_path.should == sites_path
+  		page.should have_content 'http://msu.edu/'
+  	end
   end
 end
