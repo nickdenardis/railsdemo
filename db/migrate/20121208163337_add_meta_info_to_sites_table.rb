0f626e745f520ed6f1fc5009615fcd54cc8964c1
Nick DeNardis <nick.denardis@gmail.com>
2012-12-16 13:16:44 -0500

    Fixed a migration error

--- db/migrate/20121208163337_add_meta_info_to_sites_table.rb
+++ db/migrate/20121208163337_add_meta_info_to_sites_table.rb
@@ -1,6 +1,6 @@
 class AddMetaInfoToSitesTable < ActiveRecord::Migration
   def change
-  	add_column :sites, :is_ssl, :boolean, :default => 0
+  	add_column :sites, :is_ssl, :boolean, :default => false
   	add_column :sites, :domain, :string
   	add_column :sites, :uri, :string
   end
