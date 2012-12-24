d77fd4a5bce4d9c2d0c4c22d1ef67a89cc4b461e
Nick DeNardis <nick.denardis@gmail.com>
2012-12-24 11:04:47 -0500

    Changed a little frontend style

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 	  user = User.authenticate(params[:email], params[:password])
 	  if user
 	    session[:user_id] = user.id
-	    redirect_to root_url, :notice => "Logged in!"
+	    redirect_to sites_path, :notice => "Logged in!"
 	  else
 	    flash[:error] = "Invalid email or password"
 	    render "new"
